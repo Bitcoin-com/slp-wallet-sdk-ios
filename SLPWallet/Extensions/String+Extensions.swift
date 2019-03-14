7a97f78b999f59f59bcbd2eae38bb8baf98ca2b4
jbdtky <jb@bitcoin.com>
2019-03-15 00:31:49 +0100

    Change strategy to convert String to Double

--- SLPWallet/Extensions/String+Extensions.swift
+++ SLPWallet/Extensions/String+Extensions.swift
@@ -14,6 +14,6 @@ extension String {
     }
     
     func toDouble() -> Double? {
-        return NumberFormatter().number(from: self)?.doubleValue
+        return NSDecimalNumber(string: self).doubleValue
     }
 }
