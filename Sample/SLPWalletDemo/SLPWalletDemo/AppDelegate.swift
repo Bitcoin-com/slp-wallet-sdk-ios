f69b07bc968adffbe0bb3b0ba1c42e13a038b370
jbdtky <jb@bitcoin.com>
2019-03-27 19:32:18 +0900

    :rocket: API Key + Increase release number

--- Sample/SLPWalletDemo/SLPWalletDemo/AppDelegate.swift
+++ Sample/SLPWalletDemo/SLPWalletDemo/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import SLPWallet
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        SLPWalletConfig.setRestAPIKey("slpsdkH6aIcXEApC4wXQfqqPH")
         
         let frame = UIScreen.main.bounds
         window = UIWindow(frame: frame)
